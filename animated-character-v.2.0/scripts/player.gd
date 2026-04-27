afc52a80aa1641a82fc28d83e5cb81a092762ce9
dabihdev <dabihdev@gmail.com>
2026-04-27 17:35:47 +0200

    Feat: improve script animated character v2

--- animated-character-v.2.0/scripts/player.gd
+++ animated-character-v.2.0/scripts/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var max_speed = 500.0
+@export var jump_speed = -500.0
+var crouch_speed = max_speed/3
+var current_speed = max_speed
 
 enum States {
 	IDLE,
@@ -10,7 +12,8 @@ enum States {
 	JUMP,
 	FALL,
 	CROUCH,
-	CROUCH_WALK
+	CROUCH_WALK,
+	HIT
 }
 
 var current_state = States.IDLE : set = set_state
@@ -21,34 +24,58 @@ var facing_direction = "right"
 
 # MAIN LOOP
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# When not on floor.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		if velocity.y > 0:
+			current_state = States.FALL
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# When on floor (actions)
+	else:
+		# --> IDLE state (set as default)
+		current_state = States.IDLE
+		
+		# --> JUMP state
+		if Input.is_action_just_pressed("jump"):
+			current_state = States.JUMP
+		
+		# --> CROUCH state
+		elif Input.is_action_pressed("crouch"):
+			current_state = States.CROUCH
+			
+		# --> HIT state
+		elif Input.is_action_just_pressed("attack"):
+			current_state = States.HIT
+		
+		# --> RUN state
+		elif Input.is_action_pressed("move_right") or Input.is_action_pressed("move_left"):
+			current_state = States.RUN
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
-		velocity.x = direction * SPEED
+		# if moving while crouched, go to crouch walk
+		if current_state == States.CROUCH:
+			current_state = States.CROUCH_WALK
+		
+		# update horizontal velocity
+		velocity.x = direction * current_speed
+		
+		# handle character facing direction
 		if direction > 0:
 			facing_direction = "right"
 		else:
 			facing_direction = "left"
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, current_speed)
 	
 	move_and_slide()
 
-func get_input():
-	pass # linka gli input agli stati
 
 func set_state(new_state):
 	# Update the value (necessary when using a setter)
 	current_state = new_state
+	current_speed = max_speed
 	
 	var state_name = ""
 	
@@ -59,12 +86,18 @@ func set_state(new_state):
 			state_name = "run"
 		States.JUMP:
 			state_name = "jump"
+			velocity.y = jump_speed
 		States.FALL:
 			state_name = "fall"
 		States.CROUCH:
 			state_name = "crouch"
+			current_speed = crouch_speed
 		States.CROUCH_WALK:
 			state_name = "crouch_walk"
+			current_speed = crouch_speed
+		States.HIT:
+			state_name = "hit"
+			current_speed = 0
 
 	# Combine the state name with the direction
 	var animation_to_play = state_name + "_" + facing_direction
